88333bee6f5840babac8daa07e972929bca1ab3f
Sylvain Nicolet <sylvain.nicolet@liip.ch>
2021-11-10 11:25:49 +0100

    📦 Import TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    srcDir: './',
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                }
+            }
+        },
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
+<script lang="ts" setup>
+import '../assets/tailwind.scss'
+</script>
 <template>
-  <h1>Index</h1>
+  <h1 class="text-red-500">Index</h1>
 </template>
